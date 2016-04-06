99a69dd0cc0e376b1f4fbfab831731abaa5626aa
jefor2991 <jefor2991@7JHM6Y1.Student.UGDSB.ED>
2016-04-06 14:54:45 -0400

    Client code and Classes.

---
+++ Hockey/src/testdisk/Hockey.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package testdisk;
+
+ 
+ /**
+  * The Disk class is tested.
+  */
+ public class Hockey {
+
+	public static void main(String[] args) {
+                Puck youthPuck = new Puck(4);
+                Puck adultPuck = new Puck(5.5);
+                Puck megaPuck = new Puck(1000000);
+                Puck duplicatePuck = new Puck(4);
+                
+                System.out.println(youthPuck.getDivision());
+                System.out.println(adultPuck.getDivision());
+                System.out.println(megaPuck.getDivision());
+                
+                System.out.println("The youth puck has a radius of " + youthPuck.getRadius());
+                System.out.println(youthPuck.toString());
+                 
+                if(youthPuck.equals(duplicatePuck)){
+                    System.out.println("Pucks are the same.");
+                }else{
+                    System.out.println("Pucks are different");
+                }
+	}
+}
---
+++ Hockey/src/testdisk/Puck.java
@@ -0,0 +1,97 @@
+
+package testdisk;
+
+/**
+ *
+ * @author Jeffrey Forhan
+ */
+public class Puck extends Disk{
+    
+    final private double MIN_STD_WEIGHT = 5;
+    final private double MAX_STD_WEIGHT = 5.5;
+    final private double MIN_YTH_WEIGHT = 4;
+    final private double MAX_YTH_WEIGHT = 4.5;
+    private double weight;
+    private boolean youth;
+    private boolean standard;
+    /**
+     * Description: The Puck class constructor
+     * Pre: None 
+     * Post: Creates a new puck object
+     * @param w The weight of the puck
+     */
+    Puck(double w){
+        super(1.5,1);
+        weight = w;
+        setDivision();
+    }
+    /**
+     * Description: Gets the puck's weight
+     * Pre: None
+     * Post: Gets the value of the weight variable
+     * @return the weight of the puck
+     */
+    public double getWeight(){
+        return(weight);
+    }
+    /**
+     * Description: Sets the classification of the puck
+     * Pre: None
+     * Post: Sets the classification for the puck
+     * @return the division classification of the puck
+     */
+    public String getDivision(){
+        if(youth == true){
+            return "Youth";
+        }else if(standard == true){
+            return "Standard";
+        }else{
+            return "Unofficial";
+        }
+    }
+    /**
+     * Description: Determines the class division of the puck
+     * pre: None
+     * post: Returns the puck's division classification
+     */
+    public final void setDivision(){
+        if(MIN_YTH_WEIGHT >= weight && weight <= MAX_YTH_WEIGHT){
+            youth = true;
+            standard = false;
+        }else if(MIN_STD_WEIGHT <= weight && weight <= MAX_STD_WEIGHT){
+            youth = false;
+            standard = true;
+        }else{
+            youth = false;
+            standard = false;
+        }
+    }
+    /**
+     * description: Determines if two pucks are equal
+     * Pre: None
+     * Post: Compares two pucks
+     * @param obj the object to compare to
+     * @return If the pucks are the same
+     */
+    public boolean equals(Object obj){
+        Puck otherPuck = (Puck)obj;
+        if(otherPuck.getWeight() == getWeight()){
+            return(true);
+        }else{ 
+            return(false);
+        }
+    }
+    /**
+     * description: Gives information about a puck object
+     * Pre: None
+     * Post: Returns the radius, thickness, and the weight of the puck
+     * @return A String holding a description of the puck
+     */
+    public String toString(){
+        String puckString;
+        
+        puckString = "This puck has a radius of 1.5 inches, a thickness of 1 inch, and a weight of " + getWeight();
+        
+        return puckString;
+    }
+}
